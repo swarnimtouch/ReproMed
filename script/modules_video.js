a6ab7d6734ce846be6af7c0bb67426948400095f
swarnimtouch <digital@swarnimtouch.com>
2026-04-29 12:01:51 +0530

    Update links

--- script/modules_video.js
+++ script/modules_video.js
@@ -17,17 +17,4 @@ $(document).ready(function() {
         }, 150);
     });
 
-    $('#startQuizBtn').click(function(e) {
-        e.preventDefault();
-        let $btn = $(this);
-        let originalText = $btn.text();
-        
-        $btn.text('Checking Status...').addClass('opacity-75');
-        
-        setTimeout(function() {
-            $btn.text(originalText).removeClass('opacity-75');
-            alert("Please complete at least 90% of the video to unlock the quiz.");
-        }, 800);
-    });
-
 });
